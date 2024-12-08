511a7650e5350ec6f92af1b89ab1798e3ad4ccd4
Alex <atomasllimos@gmail.com>
2024-12-08 22:35:57 +0100

    Add Tailwind CSS configuration file with dark mode and custom font settings for Montserrat in project

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'selector',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
